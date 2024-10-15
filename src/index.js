ff355d12c9558497173395780456d8b0eeb4eff1
CielSachen <manaallaire@outlook.com>
2024-10-15 12:56:39 +0800

    fix: exported object isn't immutable

--- src/index.js
+++ src/index.js
@@ -2,9 +2,9 @@ import { baseConfig } from "./base.js";
 import { reactConfig } from "./react.js";
 
 /** The collection of ESLint configs for CielSachen's Node.js projects. */
-export default {
+export default Object.freeze({
   /** The base ESLint config for CielSachen's Node.js projects. */
   base: baseConfig,
   /** An additional ESLint config for CielSachen's React.js projects. */
   react: reactConfig,
-};
+});
