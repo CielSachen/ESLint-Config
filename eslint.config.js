e4900daa0a44cceaa6dbd116717ddf185ec11b42
CielSachen <manaallaire@outlook.com>
2024-11-09 13:13:52 +0800

    style: remove comments from config and settings files

--- eslint.config.js
+++ eslint.config.js
@@ -4,10 +4,7 @@ import cielsachenConfigs from "@cielsachen/eslint-config";
 import tseslint from "typescript-eslint";
 
 export default tseslint.config(
-  // Extend a shared configuration.
   ...cielsachenConfigs.base,
-
-  // Sets the JSConfig for the TypeScript parser to use.
   {
     languageOptions: {
       parserOptions: {
@@ -16,8 +13,6 @@ export default tseslint.config(
       },
     },
   },
-
-  // Disable TypeScript-only rules because this project only uses a JSConfig.
   {
     files: ["src/*.d.ts"],
     extends: [tseslint.configs.disableTypeChecked],
