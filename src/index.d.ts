dfa05b28fbb0e3c8a14b9b96f5d499563c5a2921
CielSachen <manaallaire@outlook.com>
2024-11-09 12:58:04 +0800

    feat: replace `@typescript-eslint` types with `eslint` types
    This replaces the types from @typescript-eslint with types from eslint and removes @typescript-eslint/utils as a dependency.

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,11 +1,11 @@
-import type { TSESLint } from "@typescript-eslint/utils";
+import type { Linter } from "eslint";
 
 /** The collection of ESLint configs for CielSachen's Node.js projects. */
 declare const cielsachenConfigs: {
   /** The base ESLint config for CielSachen's Node.js projects. */
-  readonly base: Readonly<TSESLint.FlatConfig.ConfigArray>;
+  readonly base: readonly Readonly<Linter.Config>;
   /** An additional ESLint config for CielSachen's React.js projects. */
-  readonly react: Readonly<TSESLint.FlatConfig.ConfigArray>;
+  readonly react: readonly Readonly<Linter.Config>;
 };
 
 export default cielsachenConfigs;
